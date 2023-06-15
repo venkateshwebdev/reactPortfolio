5ae26ee25870d55d8e00e0aeff3fb2c0236a1955
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-06-15 08:10:27 +0530

    enabled Parallax

--- src/containers/AboutPage/About.jsx
+++ src/containers/AboutPage/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <div className='name'>Keerthi Venkatesh Sirigineedi</div>
             <div className='content'>I am a <span>Student</span> graduated in Electronics and Communiaction Engineering.</div>
             <div className='content'>I am very much intrested in {right}<span>WEB DEVELOPMENT</span>{left}<span> && SOFTWARE TECHOLOGIES</span>.</div>
-            <div>Welcome to my portfolio website! I am a passionate and dedicated web developer with a strong foundation in full-stack development. As a recent fresher, I possess a solid understanding of front-end frameworks like React and Next.js, as well as back-end technologies such as Node.js and Express.js. Additionally, I am proficient in working with databases like MongoDB and Firebase, along with languages like JavaScript and Python.</div>
+            <div className='aboutC'>Welcome to my portfolio website! I am a passionate and dedicated web developer with a strong foundation in full-stack development. As a recent fresher, I possess a solid understanding of front-end frameworks like React and Next.js, as well as back-end technologies such as Node.js and Express.js. Additionally, I am proficient in working with databases like MongoDB and Firebase, along with languages like JavaScript and Python.</div>
             <a href="#skills"><Button name=" About my Skills →"/></a>
             </div>
         </div>
--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -1,12 +1,30 @@
 import "./projects.css"
 import HeadingUnder from "../../components/HeadingDesign/HeadingUnder";
+const Card = (props)=>{
+    return(
+        <div className="projectcard">
+        <div className="dynamic"></div>
+        <iframe className="piframe" src={props.url} scrolling="no" name='iframe' title='This is my video'></iframe>
+        {/* <iframe src={props.url} height={"500px"} width={"300px"} scrolling="no" name='iframe' title='This is my video'></iframe> */}
+        <div className="projectcardcontent">
+        <h1>{props.title}</h1>
+        <p>{props.des}</p>
+        <h3>Tech Stach Used : -<span>{props.techStack}</span></h3>
+        <div><a style={{color:"inherit"}} href={props.url}>Go to website 🔗</a></div>
+        </div>
+
+    </div>
+    )
+}
 const Projects = () => {
     return ( 
         <div className="projects-container" id="projects">    
             <HeadingUnder name="Projects" />
             <div className="project-wrap">
-                {/* <a className="project-a" href="https://venkatesh-project.vercel.app/">Click Here to Check Projects 🔗 </a> */}
-
+                <div className="projectcardcontainer">
+                    <Card url="https://fashion-ed.vercel.app/" title={"Fashioned"} techStack={"Nextjs , React , Mongoose"} des={"A fun website where you can buy stuff, share ideas, and write stories."} />
+                    <Card url="https://streamzz.vercel.app/" title={"StreamZ"} techStack={"React"} des={"Watch and save your favorite videos on this YouTube-like website."} />
+                </div>
             </div>
         </div>
      );
